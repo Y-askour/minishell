0f1cb5580b75ec8cdbc2e6c8b799aabfaeb3bb6b
Abderrahim Boudounit <aboudoun@e1r7p12.1337.ma>
2022-06-04 16:36:31 +0100

    rl_replace_line

--- signal_handler.c
+++ signal_handler.c
@@ -16,7 +16,7 @@ void	signal_handler(int signal)
 {
 	(void) signal;
 	write(1, "\n", 1);
-	//rl_replace_line("", 1);
+	rl_replace_line("", 1);
 	rl_on_new_line();//Tell the update routines that we have moved onto a new (empty) line, usually after ouputting a newline.
 	rl_redisplay();//Change what's displayed on the screen to reflect the current contents of rl_line_buffer
 }
