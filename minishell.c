6616902b0c32c498c05cb1377c92afafc1af55a8
Younes Askour <pausedarkf@gmail.com>
2022-07-28 15:35:38 +0100

    fix norm in minishell.c

--- minishell.c
+++ minishell.c
@@ -6,32 +6,32 @@
 /*   By: aboudoun <aboudoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 13:35:32 by aboudoun          #+#    #+#             */
-/*   Updated: 2022/07/28 11:57:29 by yaskour          ###   ########.fr       */
+/*   Updated: 2022/07/28 15:19:29 by yaskour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"minishell.h"
 
-FILE *fPtr;
-int exit_status = 0;
+int	g_exit_status = 0;
 
-char **get_paths(char **env)
+char	**get_paths(char **env)
 {
-	t_env *shell_env; 
-	t_env *ptr;
-	char **paths;
-	char **ret;
-	int i;
+	t_env	*shell_env;
+	t_env	*ptr;
+	char	**paths;
+	char	**ret;
+	int		i;
+
 	ptr = get_env(env);
 	shell_env = ptr;
-	while(shell_env)
+	while (shell_env)
 	{
-		 if (!ft_strncmp(shell_env->name,"PATH",4))
-			 break;
+		if (!ft_strncmp(shell_env->name, "PATH", 4))
+			break ;
 		shell_env = shell_env->next;
 	}
-	paths = ft_split(shell_env->value,':');
-	while(ptr)
+	paths = ft_split(shell_env->value, ':');
+	while (ptr)
 	{
 		free(ptr->name);
 		free(ptr->value);
@@ -40,37 +40,46 @@ char **get_paths(char **env)
 		free(shell_env);
 	}
 	i = 0;
-	while(paths[i])
+	while (paths[i])
 		i++;
 	ret = malloc(sizeof(char *) * i +1);
 	i = 0;
-	while(paths[i])
+	while (paths[i])
 	{
-		ret[i] = ft_strjoin(paths[i],"/");
+		ret[i] = ft_strjoin(paths[i], "/");
 		i++;
 	}
 	ret[i] = NULL;
 	return (ret);
 }
 
-
-int	main(int ac, char **av, char **env)
+void	shllvl(t_env *g_env)
 {
-	char    *line;
-	t_token_list	*tokens;
-	t_cmd_list		*cmd_line = NULL;
-	t_env *g_env = get_env(env);
-	t_env *temp = g_env;
-	(void) av;
-	while(temp)
+	t_env	*temp;
+
+	temp = g_env;
+	while (temp)
 	{
-		if(!ft_strncmp(temp->name,"SHLVL",5))
-			break;
+		if (!ft_strncmp(temp->name, "SHLVL", 5))
+			break ;
 		temp = temp->next;
 	}
 	temp->value = ft_itoa(ft_atoi(temp->value) + 1);
+}
+
+int	main(int ac, char **av, char **env)
+{
+	char			*line;
+	t_token_list	*tokens;
+	t_cmd_list		*cmd_line;
+	t_env			*g_env;
+
+	cmd_line = NULL;
+	g_env = get_env(env);
+	(void) av;
+	shllvl(g_env);
 	if (ac != 1 || !*env)
-		return (1);	
+		return (1);
 	while (1)
 	{
 		line = display_prompt();
@@ -79,7 +88,7 @@ int	main(int ac, char **av, char **env)
 		{
 			expand(tokens, env);
 			cmd_line = parse_cmd(tokens, cmd_line);
-			run_command(cmd_line,env,g_env);
+			run_command(cmd_line, env, g_env);
 			//print_cmdline(cmd_line);
 		}
 		free_tokens(tokens);
