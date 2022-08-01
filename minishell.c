fb8d89b84c63b886cc11554038c37927a7e797ad
abderrahim boudounit <abderrahimboudounit@gmail.com>
2022-08-01 21:56:26 +0100

    adding heredoc

--- minishell.c
+++ minishell.c
@@ -6,14 +6,11 @@
 /*   By: aboudoun <aboudoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 13:35:32 by aboudoun          #+#    #+#             */
-/*   Updated: 2022/07/24 18:01:58 by aboudoun         ###   ########.fr       */
+/*   Updated: 2022/08/01 21:52:37 by aboudoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"minishell.h"
-
-FILE *fPtr;
-int exit_status = 0;
 
 char **get_paths(char **env)
 {
@@ -61,7 +58,28 @@ char **get_paths(char **env)
 	return (ret);
 }
 
+static void	is_heredoc(t_token_list *tokens)
+{
+	t_token_elem	*tmp;
+	char			*input;
 
+	tmp = tokens->head;
+	while (tmp)
+	{
+		if (tmp->type == HEREDOC)
+		{
+			if (tmp->next && tmp->next->type == WHSPACE)
+				del_node(tmp->next, tokens);
+			input = readline(">");		
+			while(ft_strncmp(input, tmp->next->value, ft_strlen(tmp->next->value)))
+			{
+				input = readline(">");
+				rl_on_new_line();
+			}
+		}
+		tmp = tmp->next;
+	}
+}
 int	main(int ac, char **av, char **env)
 {
 	char    *line;
@@ -76,11 +94,12 @@ int	main(int ac, char **av, char **env)
 	{
 		line = display_prompt();
 		tokens = lexical_analyser(line);
+		is_heredoc(tokens);
 		if (!check_syntax(tokens))
 		{
 			expand(tokens, env);
 			cmd_line = parse_cmd(tokens, cmd_line);
-			run_command(cmd_line,env);
+			//run_command(cmd_line,env);
 			print_cmdline(cmd_line);
 			free_cmd(cmd_line);
 		}
