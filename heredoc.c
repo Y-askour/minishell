5eebf874611feda258cef328bbd23e2c7b416dd5
Abderrahim Boudounit <aboudoun@e1r5p4.1337.ma>
2022-08-19 15:09:35 +0100

    handling SIGINT on heredoc

--- heredoc.c
+++ heredoc.c
@@ -6,11 +6,29 @@
 /*   By: aboudoun <aboudoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/18 11:43:56 by aboudoun          #+#    #+#             */
-/*   Updated: 2022/08/19 12:04:26 by aboudoun         ###   ########.fr       */
+/*   Updated: 2022/08/19 15:07:30 by aboudoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"minishell.h"
+
+void	signal_action(int pid, t_token_list *list, t_token_elem *node, int *fd)
+{
+	if (pid)
+	{
+		g_exit_status = pid;
+		del_node(node->next, list);
+		del_node(node, list);
+		close(fd[1]);
+		close(fd[0]);
+	}
+	else
+	{
+		close(fd[1]);
+		node->type = REDIN;
+		node->next->value = ft_itoa(fd[0]);
+	}
+}
 
 void	heredoc_signal(int signal)
 {
@@ -26,18 +44,18 @@ void	input_heredoc(int *fd, t_token_elem *node)
 	close(fd[0]);
 	input = readline(">");
 	if (!input)
-		exit(1);
+		exit(0);
 	while (ft_strncmp(input, node->next->value, \
 			ft_strlen(node->next->value) + 1))
 	{
 		ft_putstr_fd(input, fd[1]);
 		input = readline(">");
 		if (!input)
-			exit(1);
+			exit(0);
 		rl_on_new_line();
 	}
 	close(fd[1]);
-	exit(1);
+	exit(0);
 }
 
 void	is_heredoc(t_token_list *list)
@@ -45,6 +63,7 @@ void	is_heredoc(t_token_list *list)
 	t_token_elem	*node;
 	int				fd[2];
 	int				pid;
+	int				status;
 
 	node = list->head;
 	while (node)
@@ -58,11 +77,11 @@ void	is_heredoc(t_token_list *list)
 				return ;
 			if (pid == 0)
 				input_heredoc(fd, node);
-			else
-				waitpid(pid, 0, 0);
-			close(fd[1]);
-			node->type = REDIN;
-			node->next->value = ft_itoa(fd[0]);
+			waitpid(pid, &status, 0);
+			pid = WEXITSTATUS(status);
+			signal_action(pid, list, node, fd);
+			if (!node->next)
+				break ;
 		}
 		node = node->next;
 	}
--- tokenizer/tokenizer_functions.c
+++ tokenizer/tokenizer_functions.c
@@ -6,7 +6,7 @@
 /*   By: aboudoun <aboudoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 17:45:21 by aboudoun          #+#    #+#             */
-/*   Updated: 2022/08/18 12:10:17 by aboudoun         ###   ########.fr       */
+/*   Updated: 2022/08/19 14:43:05 by aboudoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ char	*is_redpip(t_token_list *tokens, char *line)
 			add_back(tokens, "<<", HEREDOC);
 			while (*(line + 2) == ' ')
 				line++;
-			line = is_word(tokens, line + 2, " \t\v\f\r");
+			line = is_word(tokens, line + 2, " |\t\v\f\r");
 			return (line);
 		}
 		add_back(tokens, "<", REDIN);
