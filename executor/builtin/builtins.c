246668eec96ee5bb873944cca521dc1b726c617c
Younes Askour <pausedarkf@gmail.com>
2022-08-23 13:11:37 +0100

    add exit

--- executor/builtin/builtins.c
+++ executor/builtin/builtins.c
@@ -6,7 +6,7 @@
 /*   By: aboudoun <aboudoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 13:48:26 by yaskour           #+#    #+#             */
-/*   Updated: 2022/08/22 18:14:08 by yaskour          ###   ########.fr       */
+/*   Updated: 2022/08/23 13:07:01 by yaskour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	run_builtins(t_cmd_elem *cmdline,char **command, t_env *env)
 	else if (!ft_strncmp(command[0], "env", 3))
 		env_f(command, env);
 	else if (!ft_strncmp(command[0], "exit", 4))
-		exit_f();
+		exit_f(command);
 	return (1);
 }
 
@@ -65,7 +65,28 @@ void	env_f(char **command, t_env *env)
 	}
 }
 
-void	exit_f(void)
+void	exit_f(char **command)
 {
-	exit(1);
+	int j;
+	int i;
+
+	j = 0;
+	if (!command[1])
+		exit(0);
+	while (command[1][j])
+	{
+		if (!ft_isdigit(command[1][j]))
+		{
+			error_handler("bash: exit : numeric argument required",255);
+			exit(255);
+		}
+		j++;
+	}
+	i = 1;
+	while(command[i])
+		i++;
+	if (i == 2)
+		exit(ft_atoi(command[1]));
+	else
+		error_handler("bash : exit : too many arguments",1);
 }
