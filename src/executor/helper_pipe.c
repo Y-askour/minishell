63d92293321e0cd2e2cf813dbabf6e949901dad5
Younes Askour <pausedarkf@gmail.com>
2022-08-25 21:58:45 +0100

    now the error raised in builins are tracked in g_exit_status

--- src/executor/helper_pipe.c
+++ src/executor/helper_pipe.c
@@ -6,25 +6,25 @@
 /*   By: aboudoun <aboudoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/02 16:28:08 by yaskour           #+#    #+#             */
-/*   Updated: 2022/08/24 20:28:50 by yaskour          ###   ########.fr       */
+/*   Updated: 2022/08/25 21:58:03 by yaskour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	delete_spaces_helper1(t_cmd_elem *head, int *i, int *n_of_arg)
-{
-	while (head->args[*i])
-	{
-		if (ft_strncmp(head->args[*i], " ", max_len(head->args[*i], " ")))
-			*n_of_arg += 1;
-		*i += 1;
-	}
-}
-
-void	helper(char ***commands, t_cmd_elem **head, int *s, int j)
-{
-	commands[*s][j] = NULL;
-	*head = (*head)->next;
-	*s += 1;
-}
+//void	delete_spaces_helper1(t_cmd_elem *head, int *i, int *n_of_arg)
+//{
+//	while (head->args[*i])
+//	{
+//		if (ft_strncmp(head->args[*i], " ", max_len(head->args[*i], " ")))
+//			*n_of_arg += 1;
+//		*i += 1;
+//	}
+//}
+//
+//void	helper(char ***commands, t_cmd_elem **head, int *s, int j)
+//{
+//	commands[*s][j] = NULL;
+//	*head = (*head)->next;
+//	*s += 1;
+//}
