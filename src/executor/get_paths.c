5275be3c75170ed2049ea7237170171e330047c2
Younes Askour <pausedarkf@gmail.com>
2022-08-25 14:42:25 +0100

    fix norm in get_paths.c

--- src/executor/get_paths.c
+++ src/executor/get_paths.c
@@ -6,7 +6,7 @@
 /*   By: aboudoun <aboudoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 14:15:51 by yaskour           #+#    #+#             */
-/*   Updated: 2022/08/24 20:27:10 by aboudoun         ###   ########.fr       */
+/*   Updated: 2022/08/25 14:35:38 by yaskour          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,33 +26,12 @@ void	add_at_end(t_env *my_env, char *name, char *value, char **env)
 	my_env->next = tmp;
 }
 
-t_env	*get_env(char **env)
+void	get_env_loop(char **env, char **splited, t_env **my_env)
 {
-	int		i;
-	t_env	*my_env;
-	char	**splited;
 	char	*tmp;
-	char	*pwd;
+	int		i;
 
-	my_env = NULL;
-	i = 0;
-	pwd = NULL;
-	if (!*env)
-	{
-		pwd = malloc(sizeof(char) * PATH_MAX);
-		getcwd(pwd, PATH_MAX);
-		env[0] = ft_strdup("SHLVL=1");
-		env[1] = ft_strjoin(ft_strjoin(ft_strdup("PWD"), ft_strdup("=")), pwd);
-		env[2] = NULL;
-	}
-	splited = ft_split(env[i], '=');
-	my_env = malloc(sizeof(t_env) * 1);
-	my_env->env = env;
-	my_env->name = splited[0];
-	my_env->value = splited[1];
-	my_env->next = NULL;
-	free(splited);
-	i++;
+	i = 1;
 	while (env[i])
 	{
 		splited = ft_split(env[i], '=');
@@ -62,9 +41,44 @@ t_env	*get_env(char **env)
 			free(splited[1]);
 			splited[1] = tmp;
 		}
-		add_at_end(my_env, splited[0], splited[1], env);
+		add_at_end(*my_env, splited[0], splited[1], env);
 		free(splited);
 		i++;
 	}
+}
+
+t_env	*get_env_helper(char **env)
+{
+	char	**splited;
+	int		i;
+	t_env	*my_env;
+
+	my_env = NULL;
+	i = 0;
+	splited = ft_split(env[i], '=');
+	my_env = malloc(sizeof(t_env) * 1);
+	my_env->env = env;
+	my_env->name = splited[0];
+	my_env->value = splited[1];
+	my_env->next = NULL;
+	free(splited);
+	i++;
+	get_env_loop(env, splited, &my_env);
 	return (my_env);
+}
+
+t_env	*get_env(char **env)
+{
+	char	*pwd;
+
+	pwd = NULL;
+	if (!*env)
+	{
+		pwd = malloc(sizeof(char) * PATH_MAX);
+		getcwd(pwd, PATH_MAX);
+		env[0] = ft_strdup("SHLVL=1");
+		env[1] = ft_strjoin(ft_strjoin(ft_strdup("PWD"), ft_strdup("=")), pwd);
+		env[2] = NULL;
+	}
+	return (get_env_helper(env));
 }
