61e7c3000ca090c274b033b9fad3f635578c8d57
Abderrahim Boudounit <aboudoun@e3r1p14.1337.ma>
2022-08-28 14:13:08 +0100

    heredoc pace fixed

--- src/minishell.c
+++ src/minishell.c
@@ -6,11 +6,32 @@
 /*   By: aboudoun <aboudoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 13:35:32 by aboudoun          #+#    #+#             */
-/*   Updated: 2022/08/28 12:36:29 by aboudoun         ###   ########.fr       */
+/*   Updated: 2022/08/28 14:12:07 by aboudoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"minishell.h"
+
+int	heredoc_error(t_token_list *list)
+{
+	t_token_elem	*node;
+
+	node = list->head;
+	while (node && node->type != HEREDOC)
+		node = node->next;
+	if (node && node->type == HEREDOC)
+	{
+		if (node->next && node->next->type == WHSPACE)
+			del_node(node->next, list);
+		if (!node->next || (node->next->type != WORD && \
+			node->next->type != DOLLAR && node->next->type != TILDE))
+		{
+			error_handler("syntax error near unexpected token `newline'", 258);
+			return (1);
+		}
+	}
+	return (0);
+}
 
 int	loop_body(char **line, t_token_list **tokens,
 		t_cmd_list **cmd_line, t_env **g_env)
@@ -22,9 +43,10 @@ int	loop_body(char **line, t_token_list **tokens,
 	if (!ft_strlen(*line))
 		return (1);
 	*tokens = tokenizer(*line);
+	if (!heredoc_error(*tokens))
+		is_heredoc(*tokens, status, *g_env);
 	if (!check_syntax(*tokens))
 	{
-		is_heredoc(*tokens, status, *g_env);
 		expand(*tokens, g_env);
 		*cmd_line = parse_cmd(*tokens, *cmd_line);
 		run_command(*cmd_line, *g_env);
